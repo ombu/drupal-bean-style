ae888696190d8540ef0585af74b594323601f2a2
Jon Duell <jon@jonduell.com>
2015-01-05 11:53:32 -0800

    Adds bootstrap slideshow style

---
+++ styles/bootstrap_slideshow/BootstrapSlideshowBeanStyle.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Bean style for bootstrap themed slideshows.
+ */
+
+
+class BootstrapSlideshowBeanStyle extends SlideshowBeanStyle {
+  protected $theme_function = 'bean_style_bootstrap_slideshow';
+}
